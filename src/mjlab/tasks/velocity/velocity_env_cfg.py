be11f758c08cff0541c1a067ef821a97fc310453
apirrone <awanegenebayo@gmail.com>
2025-10-06 20:50:04 +0200

    adding feet air time

--- src/mjlab/tasks/velocity/velocity_env_cfg.py
+++ src/mjlab/tasks/velocity/velocity_env_cfg.py
@@ -250,7 +250,7 @@ class RewardCfg:
   air_time: RewardTerm = term(
     RewardTerm,
     func=mdp.feet_air_time,
-    weight=0.0,
+    weight=1.0,
     params={
       "asset_name": "robot",
       "threshold_min": 0.05,
